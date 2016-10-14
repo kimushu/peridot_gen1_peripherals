1ea6800072c0b44d16fa131b3365c41182310a96
kimu_shu <alfvegardrisc@gmail.com>
2016-10-14 19:16:54 +0900

    Move files into HAL/

--- ip/peridot_swi/peridot_swi_sw.tcl
+++ ip/peridot_swi/peridot_swi_sw.tcl
@@ -16,8 +16,8 @@ set_sw_property isr_preemption_supported true
 set_sw_property supported_interrupt_apis "legacy_interrupt_api enhanced_interrupt_api"
 
 # Source files
-add_sw_property c_source src/peridot_swi.c
-add_sw_property include_source inc/peridot_swi.h
+add_sw_property c_source HAL/src/peridot_swi.c
+add_sw_property include_source HAL/inc/peridot_swi.h
 add_sw_property include_source inc/peridot_swi_regs.h
 
 # Supported BSP types
