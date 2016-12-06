33490dd04339c6ecdd3d63d787b854ebfa69ef7d
kimu_shu <alfvegardrisc@gmail.com>
2016-12-06 11:23:05 +0900

    Modify section's name

--- ip/peridot_swi/HAL/src/peridot_swi.c
+++ ip/peridot_swi/HAL/src/peridot_swi.c
@@ -9,7 +9,7 @@ static peridot_swi_state *swi_sp;
 #ifndef ALT_ALLOW_CODE_AT_RESET
 /* This dummy variable prevents linker from dropping startup sections */
 extern int __reset_swi;
-static const void *peridot_swi_dummy = &__reset_swi;
+__attribute__((used)) static const void *peridot_swi_dummy = &__reset_swi;
 #endif
 
 #ifdef ALT_ENHANCED_INTERRUPT_API_PRESENT
